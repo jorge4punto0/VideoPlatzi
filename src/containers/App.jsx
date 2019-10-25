c65bffb9e95a214bb447a2282ef3cc2138a218e5
jorgelisapa <jorgelisapa@gmail.com>
2019-10-25 17:11:46 -0500

    Fake API added

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -9,66 +9,33 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
-
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
   useEffect(() => {
     fetch('http://localhost:3000/initialState')
       .then((response) => response.json())
       .then((data) => setVideos(data));
   }, []);
-
-  console.log(videos);
-
   return (
     <div className='App'>
       <Header />
       <Search />
-
-      <Categories title='Mi lista'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
 
       <Categories title='Tendencias - Las preferidas del publico'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
           <CarouselItem />
         </Carousel>
       </Categories>
 
       <Categories title='lo más visto en Colombia'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
 
