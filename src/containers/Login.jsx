86ef80a0a9ef0de5cd7fef865e1a8099a7fe0308
jorgelisapa <jorgelisapa@gmail.com>
2019-10-28 08:40:07 -0500

    Container: Register - Routes with Switch

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,43 +1,53 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+import '../assets/styles/App.scss';
 import '../assets/styles/components/Login.scss';
+
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
 const Login = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Inicia sesión</h2>
-      <form className='login__container--form'>
-        <input className='input' type='text' placeholder='Correo' />
-        <input className='input' type='password' placeholder='Contraseña' />
-        <button className='button'>Iniciar sesión</button>
-        <div className='login__container--remember-me'>
-          <label>
-            <input type='checkbox' id='cbox1' value='first_checkbox' />
-            {' '}
+  <div>
+
+    <Header className='login-header' />
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia sesión</h2>
+        <form className='login__container--form'>
+          <input className='input' type='text' placeholder='Correo' />
+          <input className='input' type='password' placeholder='Contraseña' />
+          <button className='button'>Iniciar sesión</button>
+          <div className='login__container--remember-me'>
+            <label>
+              <input type='checkbox' id='cbox1' value='first_checkbox' />
+              {' '}
             Recuérdame
-          </label>
-          <a href='/'>Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img src={googleIcon} />
-          {' '}
+            </label>
+            <a href='/'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} />
+            {' '}
             Inicia sesión con Google
-        </div>
-        <div>
-          <img src={twitterIcon} />
-          {' '}
+          </div>
+          <div>
+            <img src={twitterIcon} />
+            {' '}
             Inicia sesión con Twitter
-        </div>
-      </section>
-      <p className='login__container--register'>
+          </div>
+        </section>
+        <p className='login__container--register'>
         No tienes ninguna cuenta
-        <a href=''>Regístrate</a>
-      </p>
+          <a href=''>Regístrate</a>
+        </p>
+      </section>
     </section>
-  </section>
+    <Footer className='login-footer' />
+  </div>
 );
 
 export default Login;
