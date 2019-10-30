8b27814c8a22edc2dda7250b343289db258eba51
jorgelisapa <jorgelisapa@gmail.com>
2019-10-30 15:37:40 -0500

    Integrating use of gravatar

--- src/utils/grabatar.js
+++ src/utils/grabatar.js
@@ -1,8 +1,11 @@
 import md5 from 'md5';
 
 const gravatar = (email) => {
-    const base = 'https://gravatar.com/avatar/';
-    const formatteEmail = (email).trim().toLowerCase();
-    const hash = md5(formatteEmail, {encoding: 'binary'});
-    return `${base} ${hash}`
-}
+  const base = 'https://gravatar.com/avatar/';
+  const formattedEmail = (email).trim().toLowerCase();
+  const hash = md5(formattedEmail, { encoding: 'binary' });
+  return `${base} ${hash}`;
+};
+
+export default gravatar
+;
